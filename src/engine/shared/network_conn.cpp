27088cc50f7d4bb8f87757dc4c0e9a12f167debb
FallenKN <fallen.kn@gmail.com>
2016-07-19 08:18:37 +0200

    Only clear the error string if none is given

--- src/engine/shared/network_conn.cpp
+++ src/engine/shared/network_conn.cpp
@@ -186,9 +186,12 @@ void CNetConnection::Disconnect(const char *pReason)
 		else
 			SendControl(NET_CTRLMSG_CLOSE, 0, 0);
 
-		m_ErrorString[0] = 0;
-		if(pReason)
-			str_copy(m_ErrorString, pReason, sizeof(m_ErrorString));
+		if(pReason != m_ErrorString)
+		{
+			m_ErrorString[0] = 0;
+			if(pReason)
+				str_copy(m_ErrorString, pReason, sizeof(m_ErrorString));
+		}
 	}
 
 	Reset();
